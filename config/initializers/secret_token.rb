4fb3d0f18b7192a6661cbc3eab03049b75a7bf4a
Balazs Kiss <balazsnet@gmail.com>
2012-03-01 23:42:29 +0100

    Elso demo_app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1a39174f9862af1424848dd9cc6ca0d5223a630baece1f4d0d4c30935f62439f914e6d9d652e542a30e1fba4932a60c2506074edc69fbb4a8dd1daccfa6084bf'
